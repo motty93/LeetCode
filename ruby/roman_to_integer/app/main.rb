bfb6d3e3da0ed8dcc8e83eaf9e7a6f3a2e1a9ed6
motty93 <rdwbocungelt5@gmail.com>
2024-11-12 21:22:11 +0900

    feat: roman to integer

---
+++ ruby/roman_to_integer/app/main.rb
@@ -0,0 +1,19 @@
+def roman_to_int(s)
+  h = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  }
+  sum = 0
+
+  s.each_char do |c|
+    sum += h[c.to_sym]
+  end
+  sum
+end
+
+puts roman_to_int('LVIII')
